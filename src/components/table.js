76efd608d48d83c967337b0535080406ed89b7f7
Nikhil Varma <nikhilvarma892@gmail.com>
2018-06-15 17:21:59 +0530

    Add next row and previous row feature
    Change mithril import style
    Convert view variables to state variables
    
    ISSUE: CODE COUPLING

--- src/components/table.js
+++ src/components/table.js
@@ -1,8 +1,7 @@
 const m = require("mithril");
 const Data = require("../model/Data");
 const Button = require("./Button");
-var Stream = require("mithril/stream")
-
+import s from "mithril/stream";
 Array.prototype.first = () => {
   return this && this[0];
 };
@@ -30,54 +29,92 @@ var utils = {
 var repeat = utils.repeat.bind(utils);
 var u = 0;
 var Table = {
-  controller: vnode => {
-  },
+  controller: vnode => {},
   compState: {
-    sampleVar : new Stream(234) 
+    sampleVar: s(234),
+    startIndex: 0,
+    currentIndex: s(0),
+    pageSize: 0,
+    cols: [],
+    lastIndex: 0
   },
-  update:(ref)=>{
-    let setState = () =>{ref.sampleVar = ++u}
+  update: ref => {
+    let setState = () => {
+      ref.sampleVar = ++u;
+    };
     return setState;
   },
+  setPreviousRow: ref => {
+    return () => {
+      if (ref.startIndex < 1) {
+        return;
+      }
+      --ref.startIndex;
+      --ref.lastIndex;
+      return;
+    };
+  },
+  setNextRow: ref => {
+    return () => {
+      if (ref.lastIndex >= Data.length) {
+        ref.lastIndex = Data.length;
+        ref.startIndex = ref.lastIndex - ref.pageSize;
+        return;
+      }
+      ++ref.startIndex;
+      ++ref.lastIndex;
+      return;
+    };
+  },
   view: vnode => {
-    let pageSize = vnode.attrs.pageSize;
-    let data = vnode.attrs.data;
+    vnode.state.compState.pageSize = vnode.attrs.pageSize;
     let cols = vnode.attrs.columns;
-    let startIndex = 0;
-    let currentPageIndex = 0;
-    let length = Data.length;
-    let lastIndex = startIndex + pageSize > length ? length : startIndex + pageSize;
-    let slicedData = Data.slice(startIndex, lastIndex);
-    console.log(Table)
+    vnode.state.compState.lastIndex =
+      vnode.state.compState.startIndex + vnode.state.compState.pageSize;
+    let slicedData = Data.slice(
+      vnode.state.compState.startIndex,
+      vnode.state.compState.lastIndex
+    );
+    console.log(Table);
     return (
       <div>
-        <Button value="Previous" onclick={vnode.state.update} ref={vnode.state.compState}/>
-        <Button value="Next" onclick={vnode.state.update} ref={vnode.state.compState}/>
-        {vnode.state.compState.sampleVar}
-        { <div class="table mw8 center">
-          <div class="table-header cf bg-light-gray">
-            {repeat(cols, {
-              view: v =>
-                m(
-                  "div",
-                  { class: "cell fl w-20-ns pa2 b f4 ba tc" },
-                  v.attrs.obj
-                )
-            })}
-          </div>
-          <div class="table-body">
-            <div class="table-row">
-              {repeat(slicedData, {
+        <Button
+          value="Previous"
+          onclick={vnode.state.setPreviousRow}
+          ref={vnode.state.compState}
+        />
+        <Button
+          value="Next"
+          onclick={vnode.state.setNextRow}
+          ref={vnode.state.compState}
+        />
+
+        {
+          <div class="table mw8 center">
+            <div class="table-header cf bg-light-gray">
+              {repeat(cols, {
                 view: v =>
                   m(
                     "div",
-                    { class: "cell fl w-100 w-25-ns pa2 bb br bl f3 " },
-                    v.attrs.val
+                    { class: "cell fl w-20-ns pa2 b f4 ba tc" },
+                    v.attrs.obj
                   )
               })}
             </div>
+            <div class="table-body">
+              <div class="table-row">
+                {repeat(slicedData, {
+                  view: v =>
+                    m(
+                      "div",
+                      { class: "cell fl w-100 w-25-ns pa2 bb br bl f3 " },
+                      v.attrs.val
+                    )
+                })}
+              </div>
+            </div>
           </div>
-        </div> }
+        }
       </div>
     );
   }
