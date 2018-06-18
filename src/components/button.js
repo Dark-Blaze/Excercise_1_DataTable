4bb53a84e59ee60fb2c549582e43a587111abd6e
Nikhil Varma <nikhilvarma892@gmail.com>
2018-06-18 18:51:34 +0530

    Integrate Pager library in the table component
    Change Button onclick signature to pass parameters when called rather than inside the component
    Add callback functions for onchange events for the input fields
    Change state variables to initialize on init.

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ const m = require("mithril");
 var Button = {
   view: function(vnode) {
     return (
-      <button class={vnode.attrs.class} onclick={vnode.attrs.onclick(vnode.attrs.ref)}>
+      <button class={vnode.attrs.class} onclick={vnode.attrs.onclick}>
         {vnode.attrs.value}
       </button>
     );
